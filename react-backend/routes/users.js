3a7c6a7108ef48ae0dbe8949d8b467d636c77e99
Grant Dennison <grantdennison@yahoo.co.uk>
2022-03-12 00:11:08 +0000

    create users

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,26 +1,26 @@
 var express = require("express");
 var router = express.Router();
-
+var users = [
+  {
+    id: 1,
+    username: "grant",
+  },
+  {
+    id: 2,
+    username: "Sam",
+  },
+  {
+    id: 3,
+    username: "Danny",
+  },
+];
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   // Comment out this line:
   //res.send('respond with a resource');
 
   // And insert something like this instead:
-  res.json([
-    {
-      id: 1,
-      username: "grant",
-    },
-    {
-      id: 2,
-      username: "Sam",
-    },
-    {
-      id: 3,
-      username: "John",
-    },
-  ]);
+  res.json(users);
 });
 
 module.exports = router;
